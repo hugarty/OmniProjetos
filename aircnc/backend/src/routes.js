bc8e62718c3093efc96cbbe70293076cde2164f4
hugarty <hp-ca@hotmail.com>
2019-10-02 21:31:33 -0300

    Finalizando primeira aula

--- aircnc/backend/src/routes.js
+++ aircnc/backend/src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController =  require('./controllers/BookingController')
 const multer =  require('multer')
 const uploadConfig = require('./config/upload')
 
@@ -8,7 +10,12 @@ const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
-routes.post('/spots', upload.single("thumbnail"), SpotController.store);
 
+routes.post('/spots', upload.single("thumbnail"), SpotController.store);
+routes.get('/spots', SpotController.index);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes;
